4d778052913e17bd91c92ff8b7c501de05ff33cf
meixiang474 <1436098850@qq.com>
2021-09-12 22:39:24 +0800

    feat: 添加useMount, useDebounce

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { SearchPanel } from "./search-panel";
 import { List } from "./list";
 import * as qs from "qs";
-import { cleanObject } from "utils";
+import { cleanObject, useDebounce, useMount } from "utils";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
@@ -16,28 +16,31 @@ export const ProjectListScreen = () => {
     personId: "",
   });
 
+  // 防抖处理的查询参数
+  const debouncedParam = useDebounce(param, 500);
+
   // 工程列表
   const [list, setList] = useState([]);
 
   // 同步工程列表
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(
-      async (res) => {
-        if (res.ok) {
-          setList(await res.json());
-        }
+    fetch(
+      `${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`
+    ).then(async (res) => {
+      if (res.ok) {
+        setList(await res.json());
       }
-    );
-  }, [param]);
+    });
+  }, [debouncedParam]);
 
-  // 同步负责人列表
-  useEffect(() => {
+  // 初始化负责人列表
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async (res) => {
       if (res.ok) {
         setUsers(await res.json());
       }
     });
-  }, []);
+  });
 
   return (
     <div>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 export const isFalsy = (val) => (val === 0 ? false : !val);
 
 // 过滤掉对象里的空值
@@ -10,4 +12,26 @@ export const cleanObject = (obj) => {
     }
   });
   return res;
+};
+
+// 初始化数据
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+};
+
+// 同步数据防抖处理
+export const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+    return () => clearTimeout(timer);
+  }, [value, delay]);
+
+  return debouncedValue;
 };
