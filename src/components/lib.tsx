f34ff98a11f45bb6949dfee0ecb2b11134b00c1d
meixiang474 <1436098850@qq.com>
2021-09-18 22:25:26 +0800

    feat: 处理me接口loading error状态

--- src/components/lib.tsx
+++ src/components/lib.tsx
@@ -1,4 +1,6 @@
 import styled from "@emotion/styled";
+import { Spin, Typography } from "antd";
+import { DevTools } from "jira-dev-tool";
 
 // 基础行组件
 export const Row = styled.div<{
@@ -22,3 +24,27 @@ export const Row = styled.div<{
         : undefined};
   }
 `;
+
+const FullPage = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const FullPageLoading = () => {
+  return (
+    <FullPage>
+      <Spin size="large" />
+    </FullPage>
+  );
+};
+
+export const FullPageErrorFallback = ({ error }: { error: Error | null }) => {
+  return (
+    <FullPage>
+      <DevTools />
+      <Typography.Text type="danger">{error?.message}</Typography.Text>
+    </FullPage>
+  );
+};
